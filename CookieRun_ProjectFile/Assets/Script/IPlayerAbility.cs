30455a6517de2a31d2a701db9580720a917b608e
gimgangsan <gimgansan@gmail.com>
2023-07-31 14:51:07 +0900

    player ability interface implemented

---
+++ CookieRun_ProjectFile/Assets/Script/IPlayerAbility.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPlayerAbility
+{
+    void RestoreHP(float amount);
+    void Enlarge();
+    void GetFast();
+    void GetScore(float amount);
+}
