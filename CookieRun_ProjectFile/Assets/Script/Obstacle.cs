3779a753970f7b9bf5cd56216e6ab11b880e12d6
gimgangsan <gimgansan@gmail.com>
2023-08-03 09:03:23 +0900

    use general

--- CookieRun_ProjectFile/Assets/Script/Obstacle.cs
+++ CookieRun_ProjectFile/Assets/Script/Obstacle.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour, IObjectSpeed
 {
-    Background back;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        back = GameObject.Find("Background").GetComponent<Background>();
-    }
 
     // Update is called once per frame
     void Update()
     {
-        Movement(back.speed);
+        Movement(General.Instance.back.speed);
         if (transform.position.x <= -12)
         {
             Destroy(gameObject);
@@ -26,7 +19,7 @@ public class Obstacle : MonoBehaviour, IObjectSpeed
         if (collision.CompareTag("Player"))
         {
             Player player = collision.GetComponent<Player>();
-            if (back.speed < 38 && player.IsEnlarged == false)
+            if (General.Instance.back.speed < 38 && player.IsEnlarged == false)
             {
                 player.hp -= 10;
             }
